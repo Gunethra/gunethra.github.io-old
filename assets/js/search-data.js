2aff899c05cec847c69bd2910701fd64ad8469e0
Gunethra <117150479+Gunethra@users.noreply.github.com>
2025-11-08 14:56:25 +0000

    Deploying to gh-pages from @ Gunethra/gunethra.github.io-old@b12d1181bb1381966496d9c71cacdface4c157ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,14 +17,14 @@ ninja.data = [{
             window.location.href = "/gunethra.github.io-old/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/gunethra.github.io-old/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
